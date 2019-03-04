06717be030b931e8de2ca5e70f773b792583795f
usausa <machi.pon@gmail.com>
2019-03-04 21:19:17 +0900

    Add engine work baseline

---
+++ EngineWork/Smart.Console.Engine/Console/Engine/ConsoleEngineHostBuilder.cs
@@ -0,0 +1,8 @@
+﻿namespace Smart.Console.Engine
+{
+    using Microsoft.Extensions.Hosting;
+
+    public class ConsoleEngineHostBuilder : HostBuilder
+    {
+    }
+}
---
+++ EngineWork/Smart.Console.Engine/Console/Engine/ConsoleEngineHostService.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Smart.Console.Engine
+{
+    using Microsoft.Extensions.Hosting;
+
+    public class ConsoleEngineHostService : IHostedService
+    {
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
