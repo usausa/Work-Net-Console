6cd3061b0ac0478a7bc0d676c0a96c0d2757f7ca
usausa <machi.pon@gmail.com>
2019-04-28 19:14:13 +0900

    Add sandbox project

---
+++ Sandbox/Awaiter/Awaiter/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Awaiter
+{
+    using System;
+    using System.Runtime.CompilerServices;
+    using System.Threading.Tasks;
+
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var ret = await AwaitableResultCall();
+
+            Console.WriteLine(ret);
+        }
+
+        private static AwaitableResult AwaitableResultCall()
+        {
+            return new AwaitableResult();
+        }
+    }
+
+    public class AwaitableResult
+    {
+        public AwaitableResultAwaiter GetAwaiter()
+        {
+            return new AwaitableResultAwaiter();
+        }
+    }
+
+    public class AwaitableResultAwaiter : INotifyCompletion
+    {
+        public bool IsCompleted => false;
+
+        public void OnCompleted(Action continuation)
+        {
+            continuation();
+        }
+
+        public string GetResult()
+        {
+            return "UsaUsa";
+        }
+    }
+}
