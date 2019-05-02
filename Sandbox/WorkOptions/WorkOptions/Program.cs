ed0eac42a2321ddbdedbd5e87aee1db23e8eb6d0
usausa <machi.pon@gmail.com>
2019-05-02 09:18:22 +0900

    Update work options

--- Sandbox/WorkOptions/WorkOptions/Program.cs
+++ Sandbox/WorkOptions/WorkOptions/Program.cs
@@ -1,12 +1,37 @@
-﻿using System;
-
-namespace WorkOptions
+﻿namespace WorkOptions
 {
-    class Program
+    using System.CommandLine;
+    using System.CommandLine.Builder;
+    using System.CommandLine.Invocation;
+
+    using System.Threading.Tasks;
+
+    public static class Program
     {
-        static void Main(string[] args)
+        public static async Task<int> Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var parser = new CommandLineBuilder(new Command("work-options", handler: CommandHandler.Create(typeof(Program).GetMethod(nameof(Run)))))
+                .UseVersionOption()
+                .UseHelp()
+                .UseParseDirective()
+                .UseDebugDirective()
+                .UseSuggestDirective()
+                .RegisterWithDotnetSuggest()
+                .UseParseErrorReporting()
+                .UseExceptionHandler()
+                .AddOption(new Option(new[] { "-t", "--text" }, "string option", new Argument<string>(() => null)))
+                .AddOption(new Option(new[] { "--check" }, "bool option", new Argument<bool>()))
+                .Build();
+
+            return await parser.InvokeAsync(args).ConfigureAwait(false);
+        }
+
+        public static async Task<int> Run(string text, bool check, IConsole console = null)
+        {
+            console?.Out.WriteLine($"{text}");
+            console?.Out.WriteLine($"{check}");
+
+            return await Task.FromResult(0);
         }
     }
 }
